b38c5a33bf5509019e9738031acc6ddc17da51b5
QRWells <qirui.wang@moegi.waseda.jp>
2023-05-13 19:26:25 +0900

    chap5: half complete

---
+++ model-checking-ch5/global-bottom.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="absolute bottom-0 left-0 right-0 p-2">
+        <div class="absolute right-2 bottom-0">
+            {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+        </div>
+    </footer>
+</template>
---
+++ model-checking-ch5/setup/unocss.ts
@@ -0,0 +1,8 @@
+import { defineUnoSetup } from '@slidev/types'
+
+const range = (prefix: string) => Array.from({ length: 9 }, (_, i) => `${prefix}-${100 + i * 100}`)
+// extending the builtin windicss configurations
+export default defineUnoSetup(() => ({
+    safelist:
+        range('bg-teal').concat(range('bg-emerald')).concat(range('bg-lime')).concat(range('bg-green'))
+}))
